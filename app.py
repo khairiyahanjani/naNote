366add33b83802170647eeb843c0e75b149752b7
Khai <khaisassignment@gmail.com>
2026-01-05 10:51:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ st.title("_naNote_ is :blue[cool] :sunglasses:")
 st.header("Catatan Praktik dan Kalkulasi Hasil PSA")
 
 #option
-def page_2():
+def Kalkulasi():
     st.title("Kalkulasi Hasil PSA")
 
-pg = st.navigation(["page_1.py", page_2])
+pg = st.navigation(["app.py", Kalkulasi])
+pg.run()
+
+def Catatan():
+    st.title("Mulai Catat")
+
+pg = st.navigation(["page_2.py", Catatan])
 pg.run()
