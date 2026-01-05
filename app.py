79ca9a8c81debaf630146816541642b0b19ea649
Khai <khaisassignment@gmail.com>
2026-01-05 15:26:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,15 @@ st.header("Catatan Praktik dan Kalkulasi Hasil PSA")
 expander = st.expander("🐡Apa itu naNote?")
 expander.write('''
     naNote merupakan aplikasi berbasis web yang dirancang oleh mahasiswa/i Politeknik AKA Bogor
-    untuk membantu praktikkan mencatat saat praktik dan membantu praktikkan mengkalkulasi
+    untuk membantu praktikkan mengkalkulasi
     hasil data PSA yang di dapat.
 ''')
 expander.image("https://www.microtrac.com/images/5545e7b51b645ef7b0b93088a878cd6c/1200x/max/alpha/pic-wave-ii.jpg")
+
+expander.write('''
+    dan juga membantu praktikkan mencatat data - data saat praktik berlangsung!
+''')
+expander.image("https://stuyspec-media.s3.us-east-2.amazonaws.com/rewrite_media/81ebc150-d809-11ed-952b-f5fad1466d2c.jpg")
 
 #option
 if st.button("Catatan"):
