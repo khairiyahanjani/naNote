17a0e0fb06fde9fd708a2cf97c84e47d81356187
Khai <khaisassignment@gmail.com>
2026-01-05 09:57:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,7 @@ import streamlit as st
 st.title("_naNote_ is :blue[cool] :sunglasses:")
 st.header("Catatan Praktik dan Kalkulasi Hasil PSA")
 
-st.page_link_button("Catatan Praktik", "page_1.py")
-st.page_link_button("Kalkulasi Hasil PSA", "page_2.py")
+#option
+st.page_link("app.py", label="Home", icon="🏠")
+st.page_link("Catatan Praktik", "page_1.py")
+st.page_link("Kalkulasi Hasil PSA", "page_2.py")
