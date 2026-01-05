05016f143807574672e82dc9834388af3c0bf739
Khai <khaisassignment@gmail.com>
2026-01-05 12:01:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,15 @@ st.title("_naNote_ is :blue[cool] :sunglasses:")
 st.header("Catatan Praktik dan Kalkulasi Hasil PSA")
 color = st.get_option("theme.primaryColor")
 
+#Information tentang apk
+expander = st.expander("Apa itu naNote?")
+expander.write('''
+    naNote merupakan aplikasi berbasis web yang dirancang oleh mahasiswa/i Politeknik AKA Bogor
+    untuk membantu praktikkan mencatat saat praktik dan membantu praktikkan mengkalkulasi
+    hasil data PSA yang di dapat.
+''')
+expander.image("https://www.microtrac.com/images/5545e7b51b645ef7b0b93088a878cd6c/1200x/max/alpha/pic-wave-ii.jpg")
+
 #option
 if st.button("Catatan"):
     st.switch_page("pages/catatan_1.py", query_params={"page": "catatan"})
