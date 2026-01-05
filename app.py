057e04808ee40f886dedbd3406ed46bcb4668b3e
Khai <khaisassignment@gmail.com>
2026-01-05 09:44:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.header("Catatan Praktik dan Kalkulasi Hasil PSA")
 def page_1():
     st.title("Catatan praktik")
     st.page_link("page_2.py", query_params={"page_1.py": "Catatan praktik"})
-pg = st.navigation([page_1, "page_2.py"])
+pg = st.navigation([Home, "page_2.py"])
 pg.run()
 
 
