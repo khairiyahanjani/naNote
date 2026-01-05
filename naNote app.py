830ef1abb27d563682b13e7cc7a74ced04fa477d
Khai <khaisassignment@gmail.com>
2026-01-05 07:49:10 +0700

    Update naNote app.py

--- naNote app.py
+++ naNote app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from datetime import datetime
-
+#app.py
+import streamlit as st
 st.title("This is naNote")
 st.title("_naNote_ is :blue[cool] :sunglasses:")
