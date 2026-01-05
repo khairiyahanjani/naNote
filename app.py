731c0800d96d4e764eb4a4c35b16fe0a33dcca8e
Khai <khaisassignment@gmail.com>
2026-01-05 08:25:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("This is a naNote")
-st.title("_naNote_ is :blue[cool] :sunglasses:")
+st.write("This is a naNote")
+st.write("_naNote_ is :blue[cool] :sunglasses:")
 
