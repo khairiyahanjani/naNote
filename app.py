880570531bb8b969eaf256ab0015634305335cc8
Khai <khaisassignment@gmail.com>
2026-01-05 08:46:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 import streamlit as st
 
-st.title("This is a naNote")
 st.title("_naNote_ is :blue[cool] :sunglasses:")
+st.header("Catatan Praktik")
 
+st.button("Mulai pencatatan", type="primary")
+if st.button("Catat"):
+    st.write("hmm")
+else:
+    st.write("Goodbye")
+
+#Catatan praktik
+st.page_link("your_app.py", label="Home", icon="🏠")
+st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
+st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣", disabled=True)
+st.page_link("http://www.google.com", label="Google", icon="🌎")
