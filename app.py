545eae1223743089f2247f3d7bba1d21d895ef78
Khai <khaisassignment@gmail.com>
2026-01-05 10:12:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ st.title("_naNote_ is :blue[cool] :sunglasses:")
 st.header("Catatan Praktik dan Kalkulasi Hasil PSA")
 
 #option
-st.link_button("Go to Catatan", "page_1.py")
+st.link_button("Go to Catatan", "https://github.com/khairiyahanjani/naNote/blob/main-Note/page_1.py")
