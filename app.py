1f6360541019a0564fb3f96f79e1bd61a28c3738
Khai <khaisassignment@gmail.com>
2026-01-05 10:22:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ st.title("_naNote_ is :blue[cool] :sunglasses:")
 st.header("Catatan Praktik dan Kalkulasi Hasil PSA")
 
 #option
-st.page_link("app.py")
-st.page_link("pages/page_1.py")
-st.page_link("pages/page_2.py")
+def page_1():
+    st.title("Catatan")
+    st.page_link("page_2.py", query_params={"page_1.py": "Catatan"})
+
+pg = st.navigation([page_1, "page_2.py"])
+pg.run()
