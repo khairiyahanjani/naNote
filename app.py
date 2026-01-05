5a1461d57bc86b35731f52e1d6cdaa1605ddf292
Khai <khaisassignment@gmail.com>
2026-01-05 08:02:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from datetime import datetime
+
+st.title("This is a title")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
