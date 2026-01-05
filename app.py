fb6afcb751d076f609945abc891baae4daa9d298
Khai <khaisassignment@gmail.com>
2026-01-05 10:41:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ st.title("_naNote_ is :blue[cool] :sunglasses:")
 st.header("Catatan Praktik dan Kalkulasi Hasil PSA")
 
 #option
-def page_1():
-    st.title("Page 1")
-    if st.button("Switch to Page 2"):
-        st.switch_page("page_2.py", query_params={"utm_source": "page_1"})
+def homepage():
+    st.title("Homepage")
+    if st.button("Catatan", "Kalkulasi Hasil PSA"):
+        st.switch_page("page_1.py", query_params={"appy.py": "Homepage"})
+        st.switch_page("page_2.py", query_params={"page_1.py": "Catatan"})
 
-pg = st.navigation([page_1, "page_2.py"])
+pg = st.navigation([homepage, "page_1.py"])
 pg.run()
