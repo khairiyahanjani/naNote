1b0ddea5d714a5f7f055731401dd7285e20c3e15
Khai <khaisassignment@gmail.com>
2026-01-05 11:18:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ color = st.get_option("theme.primaryColor")
 
 #option
 if st.button("Catatan"):
-    st.switch_page("page_1.py", query_params={"page_1.py": "Catatan"})
+    st.switch_page("page_1.py", query_params={"app.py": "_naNote_ is :blue[cool] :sunglasses:"})
 if st.button("Kalkulasi"):
-    st.switch_page("page_2.py", query_params={"page_2.py": "Kalkulasi"})
+    st.switch_page("page_2.py", query_params={"page_1.py": "Catatan"})
