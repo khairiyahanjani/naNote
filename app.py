bafd225e95a120ddc7b6ab23f47ab2ea30ccb4d3
Khai <khaisassignment@gmail.com>
2026-01-05 10:04:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ st.title("_naNote_ is :blue[cool] :sunglasses:")
 st.header("Catatan Praktik dan Kalkulasi Hasil PSA")
 
 #option
-st.page_link("app.py", label="Home", icon="🏠")
 st.page_link_button("Catatan Praktik", "page_1.py")
+pg = st.navigation([page_1, "page_2.py"])
+pg.run()
+
 st.page_link_button("Kalkulasi Hasil PSA", "page_2.py")
+pg = st.navigation([page_2, "page_1.py"])
+pg.run()
