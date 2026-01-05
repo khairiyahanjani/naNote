5cd165d09cb00df51c71fb59d8ec1c03c2bf70ff
Khai <khaisassignment@gmail.com>
2026-01-05 08:49:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,6 @@ st.header("Catatan Praktik")
 
 st.button("Mulai pencatatan", type="primary")
 if st.button("Catat"):
-    st.write("hmm")
-else:
-    st.write("Goodbye")
-
-#Catatan praktik
-st.page_link("your_app.py", label="Home", icon="🏠")
-st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
-st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣", disabled=True)
-st.page_link("http://www.google.com", label="Google", icon="🌎")
+    st.int(input(" "))
+else :
+    st.write("tercatat")
