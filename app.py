eeccfa856dd20e2679e030798ad3a49927c51d0d
Khai <khaisassignment@gmail.com>
2026-01-05 09:59:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ st.header("Catatan Praktik dan Kalkulasi Hasil PSA")
 
 #option
 st.page_link("app.py", label="Home", icon="🏠")
-st.page_link("Catatan Praktik", "page_1.py")
-st.page_link("Kalkulasi Hasil PSA", "page_2.py")
+st.page_link_button("Catatan Praktik", "page_1.py")
+st.page_link_button("Kalkulasi Hasil PSA", "page_2.py")
